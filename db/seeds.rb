cacd5a37bd6f2625af1d1ff994c30fc271077dc2
Hgo0123 <hcordillot@gmail.com>
2019-08-03 14:40:32 +0200

    decorate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,30 @@ Reservation.destroy_all
 	User.create(name: Faker::Name.name, email: Faker::Internet.email, phone_number: Faker::Number.leading_zero_number(digits: 10), description: Faker::Hipster.sentence(word_count: 70))
 end
 
+puts "User created"
+
+
 10.times do
 	array = ["Paris","Lyon","Marseille","Grenoble","Montcuq","Cassis","Plaisir","St-Denis","Tours","Angles"]
 	City.create(name: array.sample, zip_code: Faker::Number.number(digits: 5))
 end
 
+puts "City created"
+
 50.times do
 Listing.create(available_bed: rand(1..5) ,price: rand(10..120) ,description: Faker::JapaneseMedia::OnePiece.quote , has_wifi: Faker::Boolean.boolean, admin: User.all.sample, city: City.all.sample)
 end
 
+puts "Listing created"
+
 10.times do 
 	Reservation.create(start_date: Date.today, end_date: Faker::Date.forward(days: 90) , guest: User.all.sample, listing: Listing.all.sample)
 end
-puts "Base de données crée chef !"
+
+puts "Reservation created"
+
+puts "==================
+DATA BASE CREATED
+=================="
+
+
